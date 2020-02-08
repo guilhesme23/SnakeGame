1f9e16de1d2daf7bec3824a58bad42eca48d83f6
Guilherme <marquesguilherme3@gmail.com>
2020-02-08 13:02:35 -0300

    gameplay

--- game/game_manager.py
+++ game/game_manager.py
@@ -1,8 +1,10 @@
 import pygame
 from game.snake import Snake
+from random import randint
 
 class GameManager():
     game_over = False
+    food = None
 
     def __init__(self, root, res=20):
         self.root = root
@@ -11,7 +13,19 @@ class GameManager():
         self.rows = int(height/res)
         self.res = res
 
+        self.gen_food()
+
         self.player = Snake(self.root, (400,300), res=self.res)
+
+    def gen_food(self):
+        fx = randint(0, self.cols - 1)
+        fy = randint(0, self.rows - 1)
+        self.food = pygame.Rect(fx * self.res, fy * self.res, self.res, self.res)
+
+    def draw_food(self):
+        if self.food:
+            food_color = (200, 34, 34)
+            pygame.draw.rect(self.root, food_color, self.food)
 
     def _draw_map(self):
         cell_color = (200,200,200, 10)
@@ -22,7 +36,12 @@ class GameManager():
 
     def update(self, dt, events):
         self.player.update(dt, events)
+        head = self.player.body[-1]
+        if head.colliderect(self.food):
+            self.player.grow()
+            self.gen_food()
 
     def draw(self):
         # self._draw_map()
+        self.draw_food()
         self.player.draw()
